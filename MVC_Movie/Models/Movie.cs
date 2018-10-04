734c503430f83b22621c17388d2b1e3577d37e92
CommandoFabio <fabi0pt@hotmail.com>
2018-10-04 02:09:06 +0100

    "Controller methods and views" part of the tutorial

--- MVC_Movie/Models/Movie.cs
+++ MVC_Movie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,16 @@ namespace MVC_Movie.Models
     public class Movie
     {
         public int ID { get; set; }
+
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
